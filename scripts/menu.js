21c03e2cb6ec052b12f8e063ba3ebda37dd340d0
Eric Bear <eric.lj.bear@gmail.com>
2021-12-04 11:31:27 -0600

    add responsive design for desktop

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,8 +1,14 @@
+const mediaQuery = window.matchMedia('(max-width: 75em)');
+
 const toggleMenu = () => {
-    const menuItems = document.getElementById("my-links");
-    if (menuItems.style.display === "block") {
-        menuItems.style.display = "none";
-    } else {
-        menuItems.style.display = "block";
+    if (mediaQuery.matches) {
+        const menuItems = document.getElementById("my-links");
+        if (menuItems.style.display === "block") {
+            menuItems.style.display = "none";
+        } else {
+            menuItems.style.display = "block";
+        };
+    } else { 
+        menuitems.style.display = "flex";
     }
-}
+};
